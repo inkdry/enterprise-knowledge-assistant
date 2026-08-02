10e7104d320ffb9e2f9e5cf3b4bbe77d577f306f
inkdry <lexavision@hotmail.com>
2026-08-02 19:31:34 -0400

    Register SQL Server persistence services

--- EnterpriseKnowledge.Api/Program.cs
+++ EnterpriseKnowledge.Api/Program.cs
@@ -1,3 +1,5 @@
+using EnterpriseKnowledge.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddHealthChecks();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ EnterpriseKnowledge.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using EnterpriseKnowledge.Application.Documents;
+using EnterpriseKnowledge.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EnterpriseKnowledge.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("KnowledgeDatabase");
+
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new InvalidOperationException("The KnowledgeDatabase connection string is not configured.");
+        }
+
+        services.AddDbContext<KnowledgeDbContext>(options => options.UseSqlServer(connectionString));
+
+        services.AddScoped<IDocumentRepository, DocumentRepository>();
+
+        return services;
+    }
+}
