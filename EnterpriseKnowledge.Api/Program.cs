5c764c7e8f24662a89d586b4358cdd217a72ec2c
inkdry <lexavision@hotmail.com>
2026-08-02 13:48:59 -0400

    Add application health endpoint

--- EnterpriseKnowledge.Api/Program.cs
+++ EnterpriseKnowledge.Api/Program.cs
@@ -5,8 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -20,4 +22,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.MapHealthChecks("/health");
 app.Run();
+
